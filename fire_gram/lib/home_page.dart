fe586203d10ec427458f28357b62d15bd4ff1361
Sadie Lewis <LewisSN@hendrix.edu>
2024-10-14 20:18:30 -0500

    Removed authentication from Home page

--- fire_gram/lib/home_page.dart
+++ fire_gram/lib/home_page.dart
@@ -1,15 +1,15 @@
-import 'package:firebase_auth/firebase_auth.dart' // new
-    hide EmailAuthProvider, PhoneAuthProvider;    // new
-import 'package:flutter/material.dart';           // new
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:flutter/material.dart';
 //import 'package:gtk_flutter/feed.dart';
 //import 'package:gtk_flutter/like_selection.dart';
 
-import 'package:provider/provider.dart';          // new
+import 'package:provider/provider.dart';
 
-import 'app_state.dart';                          // new
-import 'feed.dart'; 
+import 'app_state.dart';
+import 'feed.dart';
 import 'post.dart';
-import 'src/authentication.dart';                 // new
+import 'src/authentication.dart';
 import 'src/widgets.dart';
 
 class HomePage extends StatelessWidget {
@@ -26,15 +26,15 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           // Add from here
           Consumer<ApplicationState>(
+            //make authentication individual page
             builder: (context, appState, _) => Authentication(
                 loggedIn: appState.loggedIn,
                 signOut: () {
                   FirebaseAuth.instance.signOut();
                 }),
           ),
-          ],
-          ),
-          );
-          }
-
-          }
+        ],
+      ),
+    );
+  }
+}
